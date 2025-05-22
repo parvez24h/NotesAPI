19d6cfe7f83c8d7fe3fc00e027797536b03954f0
Parvez <parvezz24h@gmail.com>
2025-05-22 19:31:32 +0600

    database connect

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config();
+
 const app = express();
 
 app.get("/",(req,res)=>{
@@ -7,7 +11,11 @@ app.get("/",(req,res)=>{
     });
 });
 
-app.listen(8080, ()=>{
-    console.log("Server Started at Port No. 8080");
-    
-})
+mongoose.connect(process.env.DATABASE_URL).then(()=>{
+    console.log("Database connect Successfuly");
+    app.listen(8080, ()=>{
+        console.log("Server Started at Port No. 8080");
+    });
+}).catch((error)=>{
+    console.log(error); 
+});
