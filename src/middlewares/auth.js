308ea776dcdbd9e3e077b9057fe067898ef7d47c
Parvez <parvezz24h@gmail.com>
2025-05-22 20:08:41 +0600

    remove Bearer

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,7 +5,6 @@ const auth = (req, res, next)=>{
     try {
         let token = req.headers.authorization;
         if(token){
-            token = token.split(" ")[1];
             let user = jwt.verify(token, SECRET_KEY);
             req.userId = user.id;
 
